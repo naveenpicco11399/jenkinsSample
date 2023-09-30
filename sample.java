7885c7db2e8c276fe1f53cfa553f74bc04a2c418
naveenpicco11399 <124562841+naveenpicco11399@users.noreply.github.com>
2023-09-30 13:29:32 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, naveen");
          System.out.println("Hello, arul");
+        System.out.println("Hello,jenkins");
     }
 }
