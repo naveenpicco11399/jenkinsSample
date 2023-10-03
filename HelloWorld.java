cab6490e711b694337e15fc36a5ba41ce612e62b
naveenpicco11399 <124562841+naveenpicco11399@users.noreply.github.com>
2023-10-03 11:42:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, naveen");
-         System.out.println("Hello, arul");
+    
         System.out.println("Hello,jenkins");
        
     }
